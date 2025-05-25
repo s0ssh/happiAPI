f04b6a4844e620d4222996c5538a0b4acd441e3b
s0ssh <me@s0s.sh>
2025-05-25 12:56:02 -0500

    Rocket boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/status")]
+fn get_status() -> &'static str {
+    "ok"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_status])
 }
